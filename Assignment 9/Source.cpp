f2a18b92d0945656b1d3c30f3586531fbef2f296
JOSHUA KILE <JOSKILE@uat.edu>
2019-04-18 07:18:34 -0700

    1.2

--- Assignment 9/Source.cpp
+++ Assignment 9/Source.cpp
@@ -3,27 +3,58 @@
 
 using namespace std;
 
+string getUsername;
+int drEvil = 100000000;
+//int& drevil = drEvil;
+string bankAccountName();
+void getBankAccount();
+
+
+
 int main()
 {
-	int login = 0;
-	string userName;
-	string drEvil = "$100,000,000";
 
-	cout << "please enter your ID" << endl;
-	cin >> userName;
+	bankAccountName();
 
-	if (userName == "dr.evil")
-	{
-		cout << "Good Day Dr.Evil, you have " << drEvil << " in your account, enjoy your evil plans." << endl;
-	}
-	else
-	{
-		if(userName != "dr.evil")
-		cout << "user not found, try again later." << endl;
-		
-	}
+	getBankAccount();
+
+
+	
 	
 	system("pause");
 
 	return 0;
+}
+
+string bankAccountName()
+{
+	//meant to be the opening to the program
+	cout << "please enter your ID" << endl;
+	cin >> getUsername;
+
+	return getUsername;
+}
+
+void getBankAccount()
+{
+	//my function that is meant to select the account of money we are meant to empty
+	if (getUsername == "drEvil")
+	{
+		cout << "Good Day Dr.Evil, your account balance: $" << drEvil << " in your account, \nenjoy your evil plans." << endl;
+
+	}
+	else if (getUsername == "drevil")
+	{
+		int& drevil = drEvil; 
+		drevil == 0;
+		cout << "recent transaction to puppy farms worldwide.\naccount balance: $" << drevil << endl;
+
+	}
+	//if the code doesn't find your account i want it to put you back to ask for a username
+	else
+	{
+		if (getUsername != "drevil" || "drEvil")
+			cout << "user not found, try again later." << endl;
+
+	}
 }
